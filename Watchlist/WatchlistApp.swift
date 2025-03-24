9b28ef26baaf624ec6006b496eeae86fff11f16c
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-24 16:44:29 +0100

    Initial Commit

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Eric on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
