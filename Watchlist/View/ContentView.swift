671b2abe69dc12e4934ac368f854f37074f72fb8
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-03-25 15:29:37 +0100

    Add accessibility label

--- Watchlist/View/ContentView.swift
+++ Watchlist/View/ContentView.swift
@@ -15,25 +15,62 @@ struct ContentView: View {
     @Query private var movies: [Movie]
 
     @State private var isSheetPresented = false
+    @State private var randomMovie = ""
+    @State private var isShowingAlert = false
+
+    // MARK: - FUNCTIONS
+
+    private func randomMovieGenerator() {
+        randomMovie = movies.randomElement()!.title
+    }
 
     var body: some View {
         List {
-            ForEach(movies) { movie in
-                HStack {
-                    Text(movie.title)
+            if !movies.isEmpty {
+                Section(
+                    header:
+                        VStack {
+                            Text("Watchlist")
+                                .font(.largeTitle.weight(.black))
+                                .foregroundStyle(.blue.gradient)
+                                .padding()
 
-                    Spacer()
-
-                    Text(movie.genre.name)
-                }
-                .swipeActions {
-                    Button(role: .destructive) {
-                        withAnimation {
-                            modelContext.delete(movie)
-                            try? modelContext.save()
+                            HStack {
+                                Label("Title", systemImage: "movieclapper")
+                                Spacer()
+                                Label("Genre", systemImage: "tag")
+                            }
                         }
-                    } label: {
-                        Label("Delete", systemImage: "trash")
+
+                ) {
+                    ForEach(movies) { movie in
+                        HStack {
+                            Text(movie.title)
+                                .font(.title2.weight(.light))
+                                .padding(.vertical, 6)
+
+                            Spacer()
+                            
+                            Text(movie.genre.name)
+                                .font(.footnote.weight(.medium))
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 3)
+                                .background(
+                                    Capsule()
+                                        .stroke(lineWidth: 1)
+                                )
+                                .foregroundStyle(.tertiary)
+                        }
+                        .swipeActions {
+                            Button(role: .destructive) {
+                                withAnimation {
+                                    modelContext.delete(movie)
+                                    try? modelContext.save()
+                                }
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
                     }
                 }
             }
@@ -45,11 +82,30 @@ struct ContentView: View {
         }
         // MARK: - SAFE AREA
         .safeAreaInset(edge: .bottom, alignment: .center) {
-            Button {
-                isSheetPresented.toggle()
-            } label: {
-                ButtonImageView(symbolName: "plus.circle.fill")
+            HStack {
+                if movies.count >= 2 {
+                    Button {
+                        randomMovieGenerator()
+                        isShowingAlert = true
+                    } label: {
+                        ButtonImageView(symbolName: "arrow.trianglehead.2.clockwise.rotate.90.circle.fill")
+                    }
+                    .alert(randomMovie, isPresented: $isShowingAlert) {
+                        Button("OK", role: .cancel) {}
+                    }
+                    .accessibilityLabel("Random Movie")
+
+                    Spacer()
+                }
+
+                Button {
+                    isSheetPresented.toggle()
+                } label: {
+                    ButtonImageView(symbolName: "plus.circle.fill")
+                }
+                .accessibilityLabel("New Movie")
             }
+            .padding(.horizontal)
         }
         // MARK: - SHEET
         .sheet(isPresented: $isSheetPresented) {
